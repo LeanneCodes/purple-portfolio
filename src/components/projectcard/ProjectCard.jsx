a563f805702fb3220c9f17127277f861f323f3b3
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2023-05-12 13:47:13 +0000

    add project card shell

--- src/components/projectcard/ProjectCard.jsx
+++ src/components/projectcard/ProjectCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './projectcard.css'
 
-const ProjectCard = () => {
+const ProjectCard = ({ img, name, tech, summary, url  }) => {
   return (
-    <div>ProjectCard</div>
+    <div className='project-card'>
+      <div>
+        <img src={img} alt="project-image" />
+      </div>
+      <div className='project-content'>
+        <div className='project-info'>
+          <h2>{name}</h2>
+          <h4>{tech}</h4>
+          <p>{summary}</p>
+        </div>
+        <button><a>{url}</a></button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './projects.css'
+import ProjectCard from '../../components/projectcard/ProjectCard'
 
 const Projects = () => {
   return (
-    <div className='guide'>Projects</div>
+    <div className='projects-container' id='projects'>
+      <div className='projects-header'>
+        <h1>My Projects</h1>
+      </div>
+      <div className='projects'>
+        <ProjectCard img="" name="Project 1" tech="JS" summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e." url="www.github.com" />
+        <ProjectCard img="" name="Project 1" tech="JS" summary="some info about the project" url="www.github.com" />
+        <ProjectCard img="" name="Project 1" tech="JS" summary="some info about the project" url="www.github.com" />
+        <ProjectCard img="" name="Project 1" tech="JS" summary="some info about the project" url="www.github.com" />
+      </div>
+    </div>
   )
 }
 
